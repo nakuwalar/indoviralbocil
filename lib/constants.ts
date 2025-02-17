0cb1ef9eb8ad0ba808f6af9c0d4cf41c7f972412
nakuwalar <nakuwalar@gmail.com>
2025-02-17 15:58:08 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const DOODSTREAM_BASE_URL =
-    process.env.DOODSTREAM_BASE_URL || "https://doodapi.com";
+    process.env.DOODSTREAM_BASE_URL || "https://filemoonapi.com";
 export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY || '459390z2ovu6s77jdi27fn';
 export const SITENAME = process.env.SITENAME || "Indo Viral Bocil";
 export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "20");
